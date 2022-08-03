56b9ebb33fef11facb2f225b13864e716b709ee2
rasel <rasel@appscode.com>
2022-08-03 19:16:41 +0600

    add comments in deployment

--- deployment/deployment.go
+++ deployment/deployment.go
@@ -11,6 +11,8 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
+//emptyDeploymentTemplate is a template that contains the basic configuration of a deployment.
+//it takes the minimum parameter of a deployment and returns a deployment
 func emptyDeploymentTemplate(deploymentName string, replicas int32, podLabel1 string, podLabel2 string, containerName string, image string, containerPortNumber int32) appsv1.Deployment {
 	//func emptyDeploymentTemplate() appsv1.Deployment {
 
@@ -49,6 +51,21 @@ func emptyDeploymentTemplate(deploymentName string, replicas int32, podLabel1 st
 	}
 }
 
+//UpdateDeployment can only update the replicas of that deployment. It takes name,
+//replicas, clientset and update that deployment
+func UpdateDeployment(deploymentName string, replicas int32, clientset kubernetes.Interface) {
+	dep := GetDeployment(deploymentName, clientset)
+	dep.Spec.Replicas = others.Int32ToPointer(replicas)
+
+	_, err := clientset.AppsV1().Deployments(v1.NamespaceDefault).Update(context.TODO(), &dep, metav1.UpdateOptions{})
+	if err != nil {
+		fmt.Println("failed to update deployment ", deploymentName)
+		panic(err)
+	}
+
+	fmt.Println("updated deployment ", deploymentName)
+}
+
 //GetDeploymentList gets the list of all the running deployments
 func GetDeploymentList(clientset kubernetes.Interface) {
 	dep_lst, err := clientset.AppsV1().Deployments(v1.NamespaceDefault).List(context.TODO(), v12.ListOptions{})
@@ -64,16 +81,19 @@ func GetDeploymentList(clientset kubernetes.Interface) {
 	}
 }
 
-func GetDeployment(name string, clientset kubernetes.Interface) {
+// GetDeployment takes name, clientset of a particular deployment and return a deployment
+func GetDeployment(name string, clientset kubernetes.Interface) appsv1.Deployment {
 	dep, err := clientset.AppsV1().Deployments(v1.NamespaceDefault).Get(context.TODO(), name, v12.GetOptions{})
 	if err != nil {
 		fmt.Println("failed to get the deployment")
 		panic(err)
 	}
+	//fmt.Println(dep.Name)
 
-	fmt.Println(dep.Name)
+	return *dep
 }
 
+//CreateDeployment creates a deployment returns nothing
 func CreateDeployment(clientset kubernetes.Interface) {
 
 	dep := emptyDeploymentTemplate("book-api-server", 1, "app", "book-server", "server-container", "superm4n/book-api-server:v0.1.3", 8080)
--- main.go
+++ main.go
@@ -8,5 +8,9 @@ import (
 func main() {
 	//deployment.GetDeploymentList()
 	//deployment.GetDeployment("book-server")
-	deployment.CreateDeployment(config.CreateNewClientSet())
+	//deployment.CreateDeployment(config.CreateNewClientSet())
+	deployment.UpdateDeployment("book-api-server", 3, config.CreateNewClientSet())
+
+	//fmt.Println(*deployment.GetDeployment("book-server", config.CreateNewClientSet()).Spec.Replicas)
+
 }
