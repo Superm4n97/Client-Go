80c5187c1c9a2c7ba70713f054015e3afce7665e
rasel <rasel@appscode.com>
2022-08-03 16:54:12 +0600

    add deployment.go

--- deployment/deployment.go
+++ deployment/deployment.go
@@ -1,1 +1,24 @@
 package deployment
+
+import (
+	"context"
+	"fmt"
+	"github.com/Superm4n97/Client-Go/config"
+	v1 "k8s.io/api/core/v1"
+	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func GetDeploymentList() {
+	clientset := config.CreateNewClientSet()
+	dep_lst, err := clientset.AppsV1().Deployments(v1.NamespaceDefault).List(context.TODO(), v12.ListOptions{})
+
+	if err != nil {
+		fmt.Println("failed to create deployment list")
+		panic(err)
+	}
+
+	fmt.Println("Deployments:")
+	for _, item := range dep_lst.Items {
+		fmt.Println(item.Name)
+	}
+}
