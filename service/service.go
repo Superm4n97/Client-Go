10f4c8cdaafe7b1443048fe52b9a4c2eaa382f47
rasel <rasel@appscode.com>
2022-08-04 11:14:16 +0600

    add CreateService and DeleteService

--- service/service.go
+++ service/service.go
@@ -5,8 +5,47 @@ import (
 	"fmt"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/kubernetes"
 )
+
+//creates a dummy template for services for given data, it returns a service type
+func emptyServiceTemplate(svcName, lb1, lb2 string, exposePort, podPort int32) v1.Service {
+	return v1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: svcName,
+			Labels: map[string]string{
+				lb1: lb2,
+			},
+		},
+		Spec: v1.ServiceSpec{
+			Ports: []v1.ServicePort{
+				{
+					Port: exposePort,
+					TargetPort: intstr.IntOrString{
+						Type:   0,
+						IntVal: podPort,
+					},
+				},
+			},
+			Selector: map[string]string{
+				lb1: lb2,
+			},
+		},
+	}
+}
+
+func CreateNewService(clientst kubernetes.Interface) {
+	svc := emptyServiceTemplate("book-api-server", "app", "book-api-server", 9090, 8080)
+
+	res, err := clientst.CoreV1().Services(v1.NamespaceDefault).Create(context.TODO(), &svc, metav1.CreateOptions{})
+	if err != nil {
+		fmt.Println("failed to create service")
+		panic(err)
+	}
+
+	fmt.Println(res.Name, " service created")
+}
 
 func GetService(svcName string, clientset kubernetes.Interface) v1.Service {
 	svc, err := clientset.CoreV1().Services(v1.NamespaceDefault).Get(context.TODO(), svcName, metav1.GetOptions{})
@@ -29,4 +68,14 @@ func GetAllServices(clientset kubernetes.Interface) {
 	for _, item := range svcList.Items {
 		fmt.Println(item.Name, " ", item.Spec.Ports)
 	}
+}
+
+func DeleteService(svcName string, clientset kubernetes.Interface) {
+	err := clientset.CoreV1().Services(v1.NamespaceDefault).Delete(context.TODO(), svcName, metav1.DeleteOptions{})
+	if err != nil {
+		fmt.Println("failed to delete the service ", svcName)
+		panic(err)
+	}
+
+	fmt.Println("service ", svcName, " deleted")
 }
